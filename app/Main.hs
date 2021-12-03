3695720a97522e4695a4d54d2ef23b6f92c2219d
wesj <wes@wesj.org>
2021-12-03 16:28:44 -0500

    day 2

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,18 @@ module Main where
 
 import Lib
 
+import Text.Printf
+
+runDay :: (Show a, Show b) => Int -> IO a -> IO b -> IO ()
+runDay n partOne partTwo = do
+	ansOne <- partOne
+	ansTwo <- partTwo
+	printf "Day %d Part 1: %s\n" n $ show ansOne
+	printf "Day %d Part 2: %s\n" n $ show ansTwo
+
+
 main :: IO ()
-main = someFunc
+main = do
+	runDay 1 Lib.day01_01 Lib.day01_02
+	runDay 2 Lib.day02_01 Lib.day02_02
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,10 @@
-module Lib
-    ( someFunc, readInput, puzzleInputFile, skip2, day01_01, day01_02
-    ) where
+module Lib where
 
 import Text.Printf
 import Debug.Trace
+import Data.Complex
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
-
+puzzleInputDir :: String
 puzzleInputDir = "/Users/wjordan/code/adventofcode21/in"
 
 puzzleInputFile :: Int -> Int -> String
@@ -25,6 +22,8 @@ testInput :: Int -> Int -> IO [String]
 testInput day part = do
     myInput <- readFile (testInputFile day part)
     return $ lines myInput
+
+---
 
 skip2 :: [a] -> [(a,a)]
 skip2 [] = []
@@ -49,7 +48,7 @@ day01_2_pure :: [Int] -> Int
 day01_2_pure ds = let
     dms = skip3 ds
     dmss = map (\(a,b,c) -> a + b + c) dms
-    in day01_1_pure (trace (show dmss) dmss)
+    in day01_1_pure dmss
 
 day01_01 :: IO Int
 day01_01 = do
@@ -62,3 +61,44 @@ day01_02 = do
     ss <- readInput 1 1
     let ds = [read s :: Int | s <- ss]
     return $ day01_2_pure ds
+
+---
+type State = (Int, Int, Int)
+type Cmd = (String, Int)
+
+zero :: State
+zero = (0, 0, 0)
+
+getCmd :: String -> Cmd
+getCmd line = let
+    ws = words line
+    v = read $ (head . tail) ws
+    in (head ws, v)
+
+readCmd :: Bool ->  State -> Cmd -> State
+readCmd False (x,y,aim) ("forward", v) = (x + v, y, aim)
+readCmd False (x,y,aim) ("up", v)  = (x, y - v, aim)
+readCmd False (x,y,aim) ("down", v) = (x, y + v, aim)
+readCmd True (x,y,aim) ("forward", v) = (x + v, y + (v * aim), aim)
+readCmd True (x,y,aim) ("up", v) = (x, y, aim - v)
+readCmd True (x,y,aim)("down", v) = (x, y, aim + v)
+
+day02_01_pure :: State -> [Cmd] -> State
+day02_01_pure = foldl (readCmd False)
+
+day02_01 :: IO Int
+day02_01 = do
+    ss <- readInput 2 1
+    let cmds = map getCmd ss
+    let (x,y,aim) = day02_01_pure zero cmds
+    return $ x * y
+
+day02_02_pure :: State -> [Cmd] -> State
+day02_02_pure = foldl (readCmd True)
+
+day02_02 :: IO Int
+day02_02 = do
+    ss <- readInput 2 1
+    let cmds = map getCmd ss
+    let (x,y,aim) = day02_02_pure zero cmds
+    return $ x * y
